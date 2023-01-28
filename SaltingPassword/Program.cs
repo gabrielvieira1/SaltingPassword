8ce161865e991875a9204914d6437ef9a98353c0
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-01-28 14:56:58 -0300

    Projeto criado

---
+++ SaltingPassword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SaltingPassword
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      PasswordWithSaltHasher pwHasher = new PasswordWithSaltHasher();
+      HashWithSaltResult hashResultSha256 = pwHasher.HashWithSalt("ultra_safe_P455w0rD", 64, SHA256.Create());
+      HashWithSaltResult hashResultSha512 = pwHasher.HashWithSalt("ultra_safe_P455w0rD", 64, SHA512.Create());
+
+      Console.WriteLine(hashResultSha256.Salt);
+      Console.WriteLine(hashResultSha256.Digest);
+      Console.WriteLine();
+      Console.WriteLine(hashResultSha512.Salt);
+      Console.WriteLine(hashResultSha512.Digest);
+    }
+  }
+}
